93cdf9c48ecbd8131020ad8f6ceea19ad026944f
manik-it-hub <maniks1516@gmail.com>
2025-07-20 20:45:06 +0530

    Initial commit - Gemini Clone React App

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
 					<>
 						<div className="greet">
 							<p>
-								<span>Hello , Dev </span>
+								<span>Hii, Buddies </span>
 							</p>
 							<p>How Can i Help You Today?</p>
 						</div>
